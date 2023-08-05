b5328c77ca763361ae657274e3590fba6a92b3a2
Oleg <olezhka.berkut@inbox.ru>
2023-08-05 16:15:58 -0700

    Example002_MaxMin3numbers

---
+++ Example002_MaxMin3num/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number3 = int.Parse(Console.ReadLine());
+if(number1 > number2 && number1 > number3)
+{
+    Console.WriteLine($"Максимум: {number1}");
+}
+else if(number2 > number1 && number2 > number3)
+{
+     Console.WriteLine($"Максимум: {number2}");
+}
+else if (number3 > number1 && number3 > number2)
+{
+    Console.WriteLine($"Максимум: {number3}");
+}
+else
+{
+    Console.WriteLine("Числа одинаковые");
+}
+
