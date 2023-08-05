09161f1acfc9b0a9a40db591d8b8e5aa33ff0d1e
Oleg <olezhka.berkut@inbox.ru>
2023-08-05 16:33:49 -0700

    Example004_1-n example4

---
+++ Example004_1-n/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(" ");
+while(number > 2)
+{
+    if(number %2 == 0)
+    {
+        number -= 2;
+        Console.WriteLine(number);
+    }
+    else 
+    {
+        number -= 1;
+        Console.WriteLine(number);
+    }
+}
